114b056a8470a035e17681a026a989ff9b07ed59
Bora Semiz <borasemiz50@gmail.com>
2026-05-07 20:02:28 +0000

    refactor the storage module

--- storage/storage.go
+++ storage/storage.go
@@ -2,36 +2,64 @@ package storage
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-type S3PutObjectAPI interface {
-	PutObject(ctx context.Context, input *s3.PutObjectInput, opts ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+const csvKey = "output.csv"
+
+type Storage interface {
+	Retrieve() (io.ReadCloser, error)
+	Save(io.Reader) error
 }
 
-func ToS3(ctx context.Context, client S3PutObjectAPI, bucket, key string, body io.Reader) error {
-	_, err := client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-		Body:   body,
-	})
-	return err
+type s3Storage struct {
+	bucket  string
+	config  aws.Config
+	client  *s3.Client
+	context context.Context
 }
 
-type S3GetObjectAPI interface {
-	GetObject(ctx context.Context, input *s3.GetObjectInput, opts ...func(*s3.Options)) (*s3.GetObjectOutput, error)
-}
-
-func FromS3(ctx context.Context, client S3GetObjectAPI, bucket, key string) (io.ReadCloser, error) {
-	out, err := client.GetObject(ctx, &s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
+func (s *s3Storage) Retrieve() (io.ReadCloser, error) {
+	out, err := s.client.GetObject(s.context, &s3.GetObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(csvKey),
 	})
 	if err != nil {
 		return nil, err
 	}
 	return out.Body, nil
+}
+
+func (s *s3Storage) Save(reader io.Reader) error {
+	_, err := s.client.PutObject(s.context, &s3.PutObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(csvKey),
+		Body:   reader,
+	})
+	return err
+}
+
+func GetStorage(ctx context.Context) (Storage, error) {
+	bucket := os.Getenv("BUCKET_NAME")
+	if bucket == "" {
+		return nil, fmt.Errorf("BUCKET_NAME env var not set")
+	}
+
+	cfg, err := awsconfig.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("aws config: %w", err)
+	}
+
+	return &s3Storage{
+		bucket:  bucket,
+		config:  cfg,
+		client:  s3.NewFromConfig(cfg),
+		context: ctx,
+	}, nil
 }
